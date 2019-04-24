dda8c9e45d9891ba58b21ef62cbc8b60804f6458
Anguandia <mikeanguandia@gmail.com>
2019-04-24 15:01:28 +0300

    Add script to write user profile page content
    This script populates the user profile page with a dummy loan records'
    list and the user account section. Breakdown;
    - A dummy sumary list of user's loans to be reused across pages that require
    any such listing
    - Another script for writting the user account page, too, reusable
    - A sorting function added in the main script file to enforce sorting of
    the list by lient's name or loan id or loan status. This function will
    work for admin sorting out user lists too
    - A filtering function added to the main script to secrch lists by a
    given key, also reusable for any required search functions
    - An intermediate function called form the written user profile page to
    select the field to sort the list by, to in turn call the sorting
    funcion

--- UI/static/scripts/main.js
+++ UI/static/scripts/main.js
@@ -5,11 +5,14 @@ function showPage(required, parents){
     //add class universal to any required class's array
     required.push('universal');
     for(var parent of parents){
-        var elements = document.getElementById(parent).children;
-        document.getElementById(parent).style.display = 'block';
+        // var elements = document.getElementById(parent).children;
+        var cont = document.getElementById(parent);
+        var elements = cont.querySelectorAll('*');
+        // document.getElementById(parent).style.display = 'block';
+        cont.style.display = 'block';
         for(var element of elements){
             //get an array of classnames of each child element
-            var classes = element.getAttribute('class');
+            if(element.hasAttribute('class')){var classes = element.getAttribute('class');
             //check if atleast a class name of current element is aamong the classes required for display
             if(classes.split(' ').filter(value=>required.includes(value)).length>0){
                 //keep menu expansion icon hidden on largescreens
@@ -31,6 +34,7 @@ function showPage(required, parents){
             } else {
                 element.style.display = 'none';
             }
+        }
         }
     }
 }
@@ -57,7 +61,7 @@ function closeMenu(){
         if(window.innerWidth>window.innerHeight){
             menu.style.width = '10%';
         } else {
-            menu.style.height = '5%';
+            menu.style.height = '30%';
             document.getElementById('options').style.display = 'block';
         }
     }
@@ -91,6 +95,79 @@ addValidation = function(element){
 function redirect(){
     var role = document.getElementById('role');
     var form = document.querySelector('form');
-    console.log(form);
     form.setAttribute('action', `${role.value}.html`);
+}
+
+// sort lists(loans/users)
+function sortData(j) {
+    var list, items, swap, i, current, next, needSwap;
+    // list = document.getElementById("all_loans");
+    list = document.querySelector('.list');
+    swap = true;
+    /*Make a loop that will continue until
+    no switching has been done:*/
+    while (swap) {
+        //start by saying: no switching is done:
+        swap = false;
+        items = list.children;
+        // Loop through all list items
+        for (i = 0; i < (items.length - 1); i++) {
+            //start by saying there should be no switching:
+            needSwap = false;
+            /*Get the two elements you want to compare,
+            one from current row and one from the next:*/
+            current = items[i].getElementsByTagName("span")[j];
+            next = items[i + 1].getElementsByTagName("span")[j];
+            // highlight sorted variable
+            current.style.background = 'aliceblue';
+            // include the last row for highlighting
+            next.style.background = 'aliceblue';
+            // check if the two items should switch place eliminate case differences by using lowercase all through:
+            if (current.innerHTML.toLowerCase() > next.innerHTML.toLowerCase()) {
+                //if so, mark as a switch and break the loop:
+                needSwap = true;
+                break;
+            }
+        }
+        if(needSwap){
+            /*If a switch has been marked, make the switch
+            and mark that a switch has been done:*/
+            items[i].parentNode.insertBefore(items[i + 1], items[i]);
+            swap = true;
+        }
+    }
+}
+
+// monitor status of sort parameter to trigger approprate sort
+function selectSort(change){
+    if(change.target.value === 'Name'){
+        sortData(0);
+    } else if(change.target.value == 'Loan id'){
+        sortData(1);
+    } else if(change.target.value == 'Status'){
+        sortData(3);
+    } else {}
+}
+
+// search loans/users by id
+function filterData(j){
+    // declare variables
+    var input, filter, list, items, field, i;
+    input = document.querySelector('.searchbox');
+    // get search key
+    filter = input.value;
+    list = document.querySelector('.list');
+    items = list.children;
+    // iterate throught each list item to locate search field
+    for (i = 0; i < items.length; i++){
+        // get search field
+        field = items[i].getElementsByTagName('span')[j];
+        // leave item display as is if current input value in search field
+        if (field.innerHTML.indexOf(filter) > -1) {
+            items[i].style.display = '';
+        } else {
+        // do not show item if input value not in search field
+        items[i].style.display = 'none';
+        }
+    }
 }
