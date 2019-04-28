71b28924fcb0a46ee80ec7f7ba9dc256251e9cee
Anguandia <mikeanguandia@gmail.com>
2019-04-28 22:48:57 +0300

    reintroduce ommitted user section on client profilepage

--- UI/static/scripts/home.js
+++ UI/static/scripts/home.js
@@ -7,5 +7,6 @@ document.write(
     <button class='dashboard _lightgreen navy' id='Draft' oclick='loadSelection("Draft"," Active Loan - Anguandia Mike", "?u_loan_180524=View_loan")'><p><a href=detail.html?u_loan_180524=view_loan style='text-decoration:none; color:navy'>*Current loan<span>26% Paid</span></a></p></button>\
     <button class='dashboard _green white' id='Completed' onclick='loadSelection("Completed", "","?u_loan_180524=view_loan")'><p>Completed<span>10</span></p></button>\
     </div>\
+    <script src='static/scripts/user.js'></script>\
     </div>\
     `);
