cba9243e725751f6ec3ac4b592f31eb00a11546a
Anguandia <mikeanguandia@gmail.com>
2019-05-08 18:07:15 +0300

    Add tests
    - Add tests for user and auth routes
    - Add tests for loan routes
    - Add some test data file to supply data for tests
    
    To test this, clone this repo, install and run mocha tests, tests should
    run and fail

---
+++ tests/testData.js
@@ -0,0 +1,57 @@
+exports.test_users = [
+    {
+        "email" : "user1@mail.com",
+        "firstName" : "test1",
+        "lastName" : "user1",
+        "password" : "user1",
+        "address" : "block1, street1, city1",
+        "tel": 256775225010,
+    },
+    {
+        "email" : "user2@mail.com",
+        "firstName" : "test2",
+        "lastName" : "user2",
+        "password" : "user2",
+        "address" : "block2, street2, city2",
+        "tel": 256775225010,
+    },
+    {
+        "email" : "user3@mail.com",
+        "firstName" : "test3",
+        "lastName" : "user3",
+        "password" : "user3",
+        "address" : "block3, street3, city3",
+        "tel": 256775225010,
+    },
+    {
+        "mail" : "user1@mail.com",
+        "firstName" : "test1",
+        "lastName" : "user1",
+        "password" : "user1",
+        "address" : "block1, street1, city1",
+        "tel": 256775225010,
+    }
+];
+
+exports.testLoans = [
+    {
+        user: 'user1@mail.com',
+        amount: 10000,
+        tenor: 12
+    },
+    {
+        user: 'user2@mail.com',
+        amount: 20000,
+        tenor: 8
+    },
+    {
+        user: 'user3@mail.com',
+        amount: 20000,
+        tenor: 6
+    },
+    {
+        user: 'user4@mail.com',
+        amount: 16000,
+        tenor: 12
+    }
+];
---
+++ tests/testUsers.js
@@ -0,0 +1,236 @@
+/* eslint-disable no-undef */
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../app');
+
+const users = require('../models/users');
+const testData = require('./testData');
+const testUsers = testData.test_users;
+
+chai.use(chaiHttp);
+should = chai.should();
+
+describe('test user end points', () => {
+    beforeEach((done) => {
+        users.slice();
+        chai.request(app).post('/auth/signup').send(testUsers[0]).end();
+        done();
+    });
+    describe('GET /users', () => {
+        // test the get routes
+        it("should delete setup user and return users' array", (done) => {
+            // test get all when empty list
+            // delete user created during setup
+            chai.request(app).delete('/users/user1@mail.com').end(done());
+            it((done) => {
+                chai.request(app)
+                .get('/users')
+                .end(res => {
+                    res.should.have.status(200);
+                    res.body.data.should.be.a('array');
+                    res.body.data.length.should.eql(0);
+                    done();
+                });
+            });
+        });
+        it("should retuen an array of all users", (done) => {
+            // test get all when list populated
+            chai.request(app)
+            .get('/users')
+            .end(res => {
+                res.should.have.status(200);
+                res.body.data.should.be.a('array');
+                res.body.data[0].email.should.eql('user1@mail.com');
+                done();
+            });
+        });
+        it('should return a single user object', (done) => {
+            // test get single user
+            const email = 'user1@mail.com';
+            chai.request(app)
+            .get(`/users/${email}`)
+            .end(res => {
+                res.should.have.status(200);
+                res.body.data.should.be.a('object');
+                res.body.data.firstName.should.eql('test1');
+                done();
+            });
+        });
+        it('should return message user unavailable', (done) => {
+            // test get unavailable user
+            let email = 'unavailable@matchMedia.com';
+            chai.request(app)
+            .get(`/users/${email}`)
+            .end(res => {
+                res.should.have.status(404);
+                res.body.should.have.property('error');
+                res.body.error.should.eql(`user with email ${email} does not exist`);
+                done();
+            });
+        });
+    });
+    describe('POST /', () => {
+        describe('user signup/registration', () => {
+            // test user creation
+            describe('should register user', () => {
+                // test successful registration
+                it('should create user, all fields supplied', (done) => {
+                    // test should register user with all fields provided and valid
+                    chai.request(app)
+                    .post('/auth/signup')
+                    .send(testUsers[1])
+                    .end(res => {
+                        res.status.should.eql(201);
+                        res.body.data.should.be.a('object');
+                        res.body.data.firstName.should.eql(testUsers[1].firstName);
+                        done();
+                    });
+                });
+                it('Create user, non mandatory fields ommitted', (done) => {
+                    // should create user with non-essential fields missing
+                    delete testUsers[0].tel;
+                    testUsers[0].email = 'new@mail.com';
+                    chai.request(app)
+                    .post('/auth/signup')
+                    .send(testUsers[0]) // no tel supplied
+                    .end(res => {
+                        res.status.should.eql(201);
+                        res.body.data.should.be.a('object');
+                        res.body.data.firstName.should.eql(testUsers[0].firstName);
+                        done();
+                    });
+                });
+            });
+            describe('Registration should fail', () => {
+                it('should fail registration - no email', (done) => {
+                    // test registration fails if no email provided
+                    delete testUsers[0].email;
+                    chai.request(app)
+                    .post('/auth/signup')
+                    .send(testUsers[0])
+                    .end(res => {
+                        res.should.have.status(400);
+                        res.body.error.should.eql('email missing');
+                        done();
+                    });
+                });
+                it('should fail - invalid user data', (done) => {
+                    // test registration fails if invalid email provided
+                    chai.request(app)
+                    .post('/auth/signup')
+                    // change testuser email to 'invalid email'
+                    .send(Object.assign(testUsers[0], {email: 'invalid email'}))
+                    .end(res => {
+                        res.should.have.status(400);
+                        res.body.error.should.eql('invalid email');
+                        done();
+                    });
+                });
+                it('should fail - invalid/missing field(emai)', (done) => {
+                    // test registration fails if invalid email provided
+                    chai.request(app)
+                    .post('/auth/signup')
+                    .send(testUsers[3]) // has intentional typo in email as mail
+                    .end(res => {
+                        res.should.have.status(400);
+                        res.body.error.should.eql('email missing');
+                        done();
+                    });
+                });
+                it('should fail - duplicate registration', (done) => {
+                    // test registration fails if email already exists
+                    chai.request(app)
+                    .post('/auth/signup')
+                    // change testuser email to user1's email
+                    .send(testUsers[0])
+                    .end(res => {
+                        res.should.have.status(400);
+                        res.body.status.should.eql(400);
+                        res.body.error.should.eql("Account 'user1@mail.com' exists, please signin");
+                        done();
+                    });
+                });
+            });
+        });
+        describe('user signin', () => {
+            // test user can signin
+            it('should signin', (done) => {
+                chai.request(app)
+                .post('/auth/signin')
+                .send({email: 'user1@mail.com', password: 'user1'})
+                .end(res => {
+                    res.should.have.status(200);
+                    res.body.data.should.have.property('token');
+                    res.body.data.firstName.should.eql('test1');
+                    done();
+                });
+            });
+            describe('Fail signin and flag errors', () => {
+                it('should not signin but alert non-existent account', (done) => {
+                    chai.request(app)
+                    .post('/auth/signin')
+                    .send({email:'noneexistent@mail.com', password: 'user1'})
+                    .end(res => {
+                        res.should.have.status(404);
+                        res.body.should.have.property('error');
+                        res.body.error.should.eql('user not found');
+                        done();
+                    });
+                });
+                it('should not signin, flag wrong password', (done) => {
+                    chai.request(app)
+                    .post('/auth/signin')
+                    .send({email:'user1@mail.com', password: 'wrong'})
+                    .end(res => {
+                        res.should.have.status(401);
+                        res.body.should.have.property('error');
+                        res.body.error.should.eql('Wrong password');
+                        done();
+                    });
+                });
+            });
+        });
+    });
+    describe('PATCH users/:user-email/verify', () => {
+        it('should change user status', done => {
+            let email = 'user1@mail.com';
+            chai.request(app)
+            .patch(`/users/${email}/verify`)
+            .send({status: 'verified'})
+            .end(res => {
+                res.should.have.status(200);
+                res.body.data.email.should.eql(email);
+                res.body.data.status.should.eql('verified');
+                done();
+            });
+        });
+        describe('should fail update, flag errors', () => {
+            it('should fail to update if invalid status', (done) => {
+                let email = 'user1@mail.com';
+                chai.request(app)
+                .patch(`/users/${email}/verify`)
+                .send({status: 'approved'}) // approved is not a valid status
+                .end(res => {
+                    res.should.have.status(400);
+                    res.body.error.should.eql(`invalid status`);
+                    done();
+                })
+            });
+            it('should fail update if user not admin')
+        });
+    });
+    describe('DELETE /<:user-email>', () => {
+        describe('should delete user', () => {
+            it.only('should delete a user if admin', (done) => {
+                let email = 'user1@mail.com';
+                chai.request(app)
+                .delete(`/users/${email}`)
+                .end(res => {
+                    // res.should.have.status(200);
+                    res.body.data.msg.should.eql(`user test1 user1 deleted`)
+                    done();
+                })
+            });
+        });
+    });
+});
