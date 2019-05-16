d79b96fcdbb87044d9659e0befb3f255826dc2b3
Anguandia <mikeanguandia@gmail.com>
2019-05-16 13:28:00 +0300

    reset local develop after corruption

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const logger = require('morgan');
+import express from 'express';
+import logger from 'morgan';
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const loansRouter = require('./routes/loans');
-const authRouter = require('./routes/auth');
+import indexRouter from './routes/index';
+import usersRouter from './routes/users';
+import loansRouter from './routes/loans';
+import authRouter from './routes/auth';
 
 const port=process.env.PORT || 3000;
 
@@ -15,8 +15,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/loans', loansRouter);
-app.use('/auth', authRouter);
+app.use('/api/v1/users', usersRouter);
+app.use('/api/v1/loans', loansRouter);
+app.use('/api/v1/auth', authRouter);
 
-module.exports = app;
+app.listen(port, ()=>{
+    console.log(`running Anguandia quick-credit on ${port}`);
+});
+
+export default app;
