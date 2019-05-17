1cd6091a47937a0535825417cef4c6f1c8774410
Anguandia <mikeanguandia@gmail.com>
2019-05-18 00:41:12 +0300

    start script

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,10 @@
 // any route that caries an access token in the request or response body is considered an authentication route while in the headers only, an authenticated/protected route
 
 import express from 'express';
+import { signup, signin, signout } from '../controllers/userController';
+import { validate } from '../utils/validation';
+
 const router = express.Router();
-import {signup, signin, signout} from '../controllers/userController';
-import {validate} from '../utils/validation';
 
 // post request for creating a user
 router.post('/signup', validate, signup);
