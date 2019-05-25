c93cadc36c595f167a291cf48ac28de072346d18
anguandia mike <mikeanguandia@gmail.com>
2019-05-25 14:55:18 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 /* eslint-disable linebreak-style */
 import express from 'express';
+import { createTables } from './utils/db';
 
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res) => {
+router.get('/', createTables, (req, res) => {
   res.json({ msg: 'Welcome to quick credit' });
 });
 
